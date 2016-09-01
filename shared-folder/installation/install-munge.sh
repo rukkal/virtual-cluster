660b9d3f9822483121277c99e72e6f1b40f2791c
rukkal <kean.mariotti@cscs.ch>
2016-09-01 15:00:36 +0200

    install-munge.sh: added security notes

--- shared-folder/installation/install-munge.sh
+++ shared-folder/installation/install-munge.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 apt-get install munge
+#WARNING: for any more serious use than this insecure toy virtual cluster a new munge.key file
+#should be generated with /usr/sbin/create-munge-key
 cp /shared-folder/installation/munge.key /etc/munge
 chown munge /etc/munge/munge.key
 chmod 400 /etc/munge/munge.key
